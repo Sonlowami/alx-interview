b86d93d1cce7b399d473ced4c6ac12737078714a
Sonlowami <uwilowami64@outlook.com>
2023-07-12 20:46:05 +0200

    Solve minimum operations interview challenge

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Contain a function minOperations that finds a the minimum number of
+operations needed to fill a file with a certain number of H letters"""
+
+
+def minOperations(n: int) -> int:
+    """Calculate minimum number of operations to fill a file with letters
+    """
+    if not isinstance(n, int):
+        return 0
+    value: int = 1
+    paste_value: int = value
+    steps: int = 0
+
+    while value < n:
+        if n % value == 0:
+            paste_value = value
+            value += paste_value
+            steps += 2
+        else:
+            value += paste_value
+            steps += 1
+    return steps
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# Minimum Operations
+
+This project contain one file in which I am prompted to find the lowest number of operations needed to write n number of H letters in a file, when all I am allowed to do is copy all and paste.
