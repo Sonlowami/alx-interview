a38ca149e004944b2d1741c99a6ca8e29983e1d6
Sonlowami <uwilowami64@outlook.com>
2023-07-19 14:01:44 +0200

    Enforce only allowed status codes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,19 +7,22 @@ regx = r'^([1-2]?\d?\d?\.?){4} - \[\d{4}(-\d{2}){2} (\d{2}:){2}\d+\.\d+\] "GET \
 status_codes = {}
 total_size = 0
 line_count = 0
+allowed_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 try:
     for line in sys.stdin:
-
+        # If an input line doesn't match the pattern, we do nothing
         if re.match(regx, line):
             status_and_size = re.search(r'\d+ \d+$', line).group().split(' ')
+            status = int(status_and_size[0])
+            size = int(status_and_size[1])
 
-            if not status_codes.get(status_and_size[0]):
-                status_codes.update({status_and_size[0]: 1})
+            if not status_codes.get(status) and status in allowed_codes:
+                status_codes.update({status: 1})
 
             else:
-                status_codes.update({status_and_size[0]:
-                                     status_codes[status_and_size[0]] + 1})
-            total_size += int(status_and_size[1])
+                status_codes.update({status:
+                                     status_codes[status] + 1})
+            total_size += size
             line_count += 1
 
         if line_count % 10 == 0:
